6952bfcfbc26374f44cd60c93dfa703544668a27
SonkeWohler <sonke.a.wohler@gmail.com>
2020-12-20 14:05:57 +0200

    corrected 02/12/20 advent of code first part

--- perl/adventOfCode_02_12_20_first.pl
+++ perl/adventOfCode_02_12_20_first.pl
@@ -9,21 +9,20 @@ my @split;
 my @formattedLine;
 
 while (<$in>) {
-  ##$line = $_;
   @split=split(" ",$_);
   $split[1] =~ s/://;
   @formattedLine = (split('-',$split[0]), @split[1..$#split]);
-  ##print"$line: @formattedLine\n";
   push(@lines, "$formattedLine[0],$formattedLine[1],$formattedLine[2],$formattedLine[3]");
 }
-print" ------- \nslurped formatted lines\n";
 
 my $i;
 my $j=0;
-foreach $line (@lines) {
-  @split = split(",",$line);
-  $i = $split[3] =~ tr/$split[2]//;
-  print"$split[3] should contain $split[2] like: $split[0] <= $i <= $split[1]\nthe original line is `$line`\n";
+foreach (@lines) {
+  @split = split(",",$_);
+  $i = 0;
+  for (split //, $split[3]) {
+    $i++ if $_ eq $split[2];
+  }
   $j++ if $split[0]<=$i<=$split[1];
 }
 my $temp = @lines;
