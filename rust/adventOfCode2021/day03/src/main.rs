54289137997396a90b344678b2c5f1911d21ff2a
sun <sonke.a.wohler@gmail.com>
2021-12-06 15:52:35 +0200

    advent of code 2021 day 3 part 2

--- rust/adventOfCode2021/day03/src/main.rs
+++ rust/adventOfCode2021/day03/src/main.rs
@@ -22,14 +22,16 @@ fn get_pivot_position(pivot: u32, previous_pivots: &[u32], vector: &[u32]) -> us
     position
 }
 
-fn get_oxygen(mut pivot: u32, pivots: &mut Vec<u32>, entries: &[u32]) {
-    if pivot == 0 { return } else { pivot = pivot - 1 };
+fn get_oxy_co2(mut pivot: u32, pivots: &mut Vec<u32>, entries: &[u32], oxy_false_co2_true: bool) -> u32 {
+    if entries.len() == 1 { return entries[0]; };
+    pivot = pivot - 1;
     let position = get_pivot_position(pivot, pivots, entries);
-    if position <= entries.len() / 2 {
+    let more_1s = position <= entries.len() / 2;
+    if more_1s ^ oxy_false_co2_true {
         pivots.push(pivot);
-        get_oxygen(pivot, pivots, &entries[position..]);
+        return get_oxy_co2(pivot, pivots, &entries[position..], oxy_false_co2_true);
     } else {
-        get_oxygen(pivot, pivots, &entries[..position]);
+        return get_oxy_co2(pivot, pivots, &entries[..position], oxy_false_co2_true);
     }
 }
 
@@ -44,6 +46,8 @@ fn main() {
     let mut digits = Vec::new();   // count the number of '1' for each digit
     let mut count_lines = 0;
     let mut entries: BinaryHeap<u32> = BinaryHeap::new();
+    // https://turreta.com/2019/09/07/rust-how-to-check-for-null-values/
+    let mut line_length: Option<usize> = None;
 
     for line in reader.lines() {
 
@@ -52,6 +56,10 @@ fn main() {
         digits.resize(line.len(), 0);
         // https://stackoverflow.com/questions/27606616/is-there-anything-in-rust-to-convert-a-binary-string-to-an-integer
         entries.push(u32::from_str_radix(&line, 2).unwrap());
+        match line_length {
+            None => line_length = Some(line.len()),
+            Some(length) => if length != line.len() { panic!("line lengths are inconsistent within the file"); },
+        }
 
         for (i, digit) in line.chars().enumerate() {
             if digit == '1' {
@@ -61,7 +69,9 @@ fn main() {
 
     }
 
-    println!("There was a total of {} lines", count_lines);
+    let line_length = line_length.unwrap();
+
+    println!("There was a total of {} lines of length {}", count_lines, line_length);
 
     let mut gamma = String::from("");
     let mut epsilon = String::from("");
@@ -88,16 +98,26 @@ fn main() {
 
     let entries = entries.into_sorted_vec();
 
-    let mut pivots_oxygen: Vec<u32> = Vec::from([]);
-    let pivot: u32 = 12;  // the input has 12 bits
-    get_oxygen(pivot, &mut pivots_oxygen, entries.as_slice());
-    //let position = get_pivot_position(pivot, entries[..].to_vec());    // I don't think this is so great
+    let mut pivots: Vec<u32> = Vec::from([]);
+    let pivot: u32 = line_length as u32;
+    let oxy = get_oxy_co2(pivot, &mut pivots, entries.as_slice(), false);
 
     let mut pivot = 0;
-    for i in pivots_oxygen {
+    for i in pivots {
         pivot = pivot + 2_u32.pow(i);
     }
-    println!("The pivots for oxygen have been {}", format!("{:b}", pivot));
-    //println!("The closest value to 2^{} is {} at position {}", pivot, entries[position], position);
+    // format from https://www.programming-idioms.org/idiom/76/binary-digits-from-an-integer/629/rust
+    println!("The pivots for oxygen have been {} which leads us to the value {}", format!("{:b}", pivot), oxy);
     
+    let mut pivots: Vec<u32> = Vec::from([]);
+    let pivot: u32 = line_length as u32;
+    let co2 = get_oxy_co2(pivot, &mut pivots, entries.as_slice(), true);
+
+    let mut pivot = 0;
+    for i in pivots {
+        pivot = pivot + 2_u32.pow(i);
+    }
+    println!("The pivots for CO2 have been {} which leads us to the value {}", format!("{:b}", pivot), co2);
+
+    println!("This makes ofr a life support rating of {}", co2 * oxy);
 }
