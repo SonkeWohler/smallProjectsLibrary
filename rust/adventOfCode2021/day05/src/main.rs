7565c1ee399843b4f5ecc0583207f6be1c55361b
sun <sonke.a.wohler@gmail.com>
2021-12-16 01:32:34 +0200

    use structs instead for data

--- rust/adventOfCode2021/day05/src/main.rs
+++ rust/adventOfCode2021/day05/src/main.rs
@@ -1,6 +1,29 @@
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 use std::str::SplitWhitespace;
+use std::fmt;
+
+struct Position {
+    x: u32,
+    y: u32,
+}
+// https://doc.rust-lang.org/std/fmt/trait.Display.html
+impl fmt::Display for Position {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+    
+}
+
+struct VentLine {
+    start: Position,
+    end: Position,
+}
+impl fmt::Display for VentLine {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}, -> {}", self.start, self.end)
+    }
+}
 
 // I don't seem to understand enough about lifetimes just yet to do this with fewer lines of code,
 // so I do it this way.
@@ -8,12 +31,15 @@ use std::str::SplitWhitespace;
 // since you are passing a reference to the original string to another routing, so to make sure it
 // doesn't go out of scope and causes a memory error Rust requires you to put lifetimes on the
 // reference that somehow makes sure.
-fn get_positions_from_input(mut input: SplitWhitespace) -> ((u32, u32), (u32, u32)) {
+fn get_positions_from_input(mut input: SplitWhitespace) -> VentLine {
     let start = input.next().unwrap();
     let mut start = start.split(',');
     let start = (start.next(), start.next() );
     let start = (start.0.unwrap(), start.1.unwrap());
-    let start: (u32, u32) = (start.0.parse().unwrap(), start.1.parse().unwrap());
+    let start = Position {
+        x: start.0.parse().unwrap(),
+        y: start.1.parse().unwrap(),
+    };
 
     // skip this part
     input.next();
@@ -22,27 +48,38 @@ fn get_positions_from_input(mut input: SplitWhitespace) -> ((u32, u32), (u32, u3
     let mut end = end.split(',');
     let end = (end.next(), end.next() );
     let end = (end.0.unwrap(), end.1.unwrap());
-    let end: (u32, u32) = (end.0.parse().unwrap(), end.1.parse().unwrap());
+    let end = Position {
+        x: end.0.parse().unwrap(),
+        y: end.1.parse().unwrap(),
+    };
 
-    (start, end)
+    VentLine {
+        start,
+        end,
+    }
 
 }
+
 
 fn main() {
     let filename = "input.txt";
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
+    let mut positions: Vec<VentLine> = Vec::new();
+
     for line in reader.lines() {
         let line = line.unwrap();   // if this fails the program might as well
         let line = line.trim().split_whitespace();
 
-        let positions = get_positions_from_input(line);
-
-        println!("{:?} -> {:?}", positions.0, positions.1);
+        let line = get_positions_from_input(line);
+        positions.push(line);
 
     }
 
+    for position in positions {
+        println!("{}", position);
+    }
 
 
 }
